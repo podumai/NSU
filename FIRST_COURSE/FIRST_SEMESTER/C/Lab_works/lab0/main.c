e5497b8cfc70f527cdf456a9fa9fcb96517190c8
Kirill <148055984+podumai@users.noreply.github.com>
2023-10-26 23:45:47 +0700

    Update main.c

--- FIRST_COURSE/FIRST_SEMESTER/C/Lab_works/lab0/main.c
+++ FIRST_COURSE/FIRST_SEMESTER/C/Lab_works/lab0/main.c
@@ -1,39 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
 #define SIZE 100
 
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 
-int check_str(char * str, int a) {
-    int count = 0, dot = 0;
+void upper_case(char * str) {
+    for (int i = 0; i < strlen(str); i++)
+        if (str[i] >= 'a' && str[i] <= 'f')
+            str[i] = toupper(str[i]);
+}
 
-   if (a >= 2 && a <= 10) {
-       for (int i = 0; i < strlen(str); i++) {
-           if (str[i] < a + 48 && str[i] >= '0')
-               count++;
-           else if (str[i] == '.')
-               dot++;
-       }
-   }
-   else if (a >= 11 && a <= 16) {
-       for (int i = 0; i < strlen(str); i++) {
-           if ((str[i] < a + 55 && str[i] >= 'A') ||
-           (str[i] >= '0' && str[i] <= '9'))
-               count++;
-           else if (str[i] == '.')
-               dot++;
-       }
-   }
-
-    if (count + dot == strlen(str) && dot < 2 && ((str[0] != '.' ||
-    str[strlen(str) - 1] != '.') || str[0] != '0'))
-        return 1;
-
-    return 0;
+void cln(char * str) {
+    str[0] = '\0';
 }
 
 
@@ -46,6 +28,19 @@ int check_dot(char * str) {
 }
 
 
+void zeroes(char * str) {
+    int count = 0;
+
+    if (check_dot(str))
+        for (int i = check_dot(str) + 1; i < strlen(str); i++)
+            if (str[i] == '0')
+                count++;
+
+    if (strlen(str) - (check_dot(str) + 1) == count)
+        str[check_dot(str)] = '\0';
+}
+
+
 int check_nums(int a, int b) {
     if (a >= 2 && a <= 16 && b >= 2 && b <= 16)
         return 1;
@@ -54,139 +49,151 @@ int check_nums(int a, int b) {
 }
 
 
-int upper_case(char * str) {
-    for (int i = 0; i < strlen(str); i++)
-        if (str[i] >= 'a' && str[i] <= 'f')
-            str[i] = toupper(str[i]);
+int check_str(char * str, int a) {
+    int count = 0, dot = 0;
+
+    if (check_dot(str) || str[0] == '.') {
+        int zero = 0;
+        int count_b = 1;
+
+        for (int i = 0; i < strlen(str); i++) {
+            if (i > check_dot(str) && str[i] == '0')
+                zero++;
+            else if (i < check_dot(str) && str[i] != '0')
+                count_b++;
+        }
+
+        if (zero + check_dot(str) + 1 == strlen(str) && count_b - 1 == 0)
+            return 0;
+    }
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] < a + '0')
+            count++;
+        else if (str[i] == '.' && dot < 3)
+            dot++;
+    }
+
+    if (a > 9)
+        for (int i = 0; i < strlen(str); i++)
+            if (str[i] >= 'A' && str[i] < a + '7')
+                count++;
+
+    if (!dot && count == strlen(str))
+        return 1;
+    else if (dot < 2 && count + dot == strlen(str))
+        return 1;
 
     return 0;
 }
 
-char num_system10(char * str1, char * str2, int a) {
-    int sum_i = 0, j = -1;
-    double sum_d = 0.;
 
-    if (!check_dot(str1)) {
-        for (int i = strlen(str1) - 1; i >= 0; i--) {
-            j++;
+void num_sys10(char * str, int a) {
+    zeroes(str);
 
-            if (str1[i] >= 'A' && str1[i] <= 'F')
-                sum_i += (str1[i] - '7') * pow(a, j);
+    if (!check_dot(str)) {
+        long long int sum = 0;
+        int j = strlen(str);
+
+        for (int i = 0; i < strlen(str); i++) {
+            j--;
+
+            if (str[i] <= '9')
+                sum += (str[i] - '0') * pow(a, j);
             else
-                sum_i += (str1[i] - '0') * pow(a, j);
+                sum += (str[i] - '7') * pow(a, j);
         }
 
-        snprintf(str2, sizeof(str2), "%d", sum_i);
+        cln(str);
+        _snprintf(str, SIZE, "%lld", sum);
     }
     else {
-        for (int i = check_dot(str1) - 1; i >= 0; i--) {
-            j++;
-            if (str1[i] >= 'A' && str1[i] <= 'F')
-                sum_d += (str1[i] - '7') * pow(a, j);
+        int j = check_dot(str);
+        double sum_d = 0.;
+
+        for (int i = 0; i < check_dot(str); i++) {
+            j--;
+
+            if (str[i] <= '9')
+                sum_d += (str[i] - '0') * pow(a, j);
             else
-                sum_d += (str1[i] - '0') * pow(a, j);
+                sum_d += (str[i] - '7') * pow(a, j);
         }
+
         j = 0;
 
-        for (int i = check_dot(str1) + 1; i < strlen(str1); i++) {
+        for (int i = check_dot(str) + 1; i < strlen(str); i++) {
             j--;
-            if (str1[i] >= 'A' && str1[i] <= 'F')
-                sum_d += (str1[i] - '7') * pow(a, j);
+
+            if (str[i] <= '9')
+                sum_d += (str[i] - '0') * pow(a, j);
             else
-                sum_d += (str1[i] - '0') * pow(a, j);
-
-        snprintf(str2, sizeof(str2), "%lf", sum_d);
+                sum_d += (str[i] - '7') * pow(a, j);
         }
+
+        cln(str);
+        _snprintf(str, SIZE, "%lf", sum_d);
     }
-
-    return *str2;
 }
 
 
-int cln(char * str) {
-    for (int i = 0; i < strlen(str) - 1; i++)
-        str[i] = ' ';
+void num_sys(char * str, int a) {
+    long long int num = atoll(str), num_str = 0;
+    long long int t[SIZE];
+    int i = 0;
 
-    return 0;
-}
-
-
-int put_str(char * str) {
-    for (int i = 0; i < strlen(str); i++) {
-        if ((str[i] < '0' || str[i] > 'F' || (str[i] > '9' && str[i] < 'A')) && str[i] != '.')
-            return 0;
-
-        putchar(str[i]);
-    }
-
-    return 0;
-}
-
-
-double num_system(char * str, int a) {
-    char * strend;
-    int t[SIZE];
-    int i = 0, num_str = 0, dot = check_dot(str), num = atoi(str);
-    double num_e = strtod(str, strend);
-
-    cln(str);
+    if (num == 0)
+        putchar(num + '0');
 
     while (num != 0) {
         num_str = num % a;
-        t[i] = num_str;
-        i++;
-        num /= a;
-
-        if (num / a == 0)
-            t[i] = num;
+        num = num / a;
+        t[i++] = num_str;
     }
 
-    int k = 0;
-
     for (int j = i - 1; j >= 0; j--) {
-        if (t[j] >= 0 && t[j] <= 9)
+        if (t[j] < 10)
             putchar(t[j] + '0');
         else
             putchar(t[j] + '7');
-
-        k++;
     }
 
-    if (dot) {
-        num_e -= num;
+    if (check_dot(str)) {
+        char * strend = 0;
+        long double num_d = strtold(str, strend);
+
+        num = atoll(str);
+        num_d -= num;
+
         putchar('.');
 
-        for (int j = 0; j < 12; j++) {
-            num_e *= a;
-            num = num_e;
-            if (num >= 0 && num <= 9)
+        for (i = 0; i < 12; i++) {
+            num_d *= a;
+            num = num_d;
+
+            if (num < 10)
                 putchar(num + '0');
             else
                 putchar(num + '7');
-            num_e -= num;
         }
     }
-
-    return *str;
 }
 
-
-void main() {
+int main() {
+    char str[SIZE];
     int b1, b2;
-    char str[SIZE], str_n[SIZE];
 
-    scanf("%d%d\n", &b1, &b2);
+    scanf_s("%d%d\n", &b1, &b2);
     fgets(str, sizeof(str), stdin);
     str[strcspn(str, "\n")] = 0;
     upper_case(str);
 
-    if (check_nums(b1, b2) == 1 && check_str(str, b1) == 1) {
-            num_system10(str, str_n, b1);
-            num_system(str_n, b2);
-            //put_str(str_n);
+    if (check_nums(b1, b2) && check_str(str, b1)) {
+        num_sys10(str, b1);
+        num_sys(str, b2);
     }
     else
         printf("bad input");
 
-    exit(0);
+    return 0;
 }
