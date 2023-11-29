4476df63803bfe619b03715e124ff4e96a8d224f
Kirill <148055984+podumai@users.noreply.github.com>
2023-11-29 19:10:51 +0700

    Update 1.2.c

--- FIRST_COURSE/FIRST_SEMESTER/C/Skillspace/Module2/1.2.c
+++ FIRST_COURSE/FIRST_SEMESTER/C/Skillspace/Module2/1.2.c
@@ -4,22 +4,13 @@
 #include <math.h>
 
 
-void scanArr(int * arr, int lenArr) {
+void scanArr(int* arr, int lenArr) {
     for (int i = 0; i < lenArr; i++)
         scanf_s("%d", &arr[i]);
 }
 
 
-int maxNum(int * arr, int lenArr) {
-    int max = 0;
-    for (int i = 0; i < lenArr; i++)
-        if (max < arr[i])
-            max = arr[i];
-    return max;
-}
-
-
-int minNum(int * arr, int lenArr) {
+int minNum(int* arr, int lenArr) {
     int min = 10000000;
     for (int i = 0; i < lenArr; i++)
         if (min > arr[i] && arr[i])
@@ -28,19 +19,17 @@ int minNum(int * arr, int lenArr) {
 }
 
 
-void printGCD(int * arr, int lenArr) {
-    int count = 0, k = 0;
-    int * dividers = (int*)malloc(sizeof(int) * (minNum(arr, lenArr)));
+void printGCD(int* arr, int lenArr) {
+    int count = 0, max = 0;
     for (int i = 1; i <= minNum(arr, lenArr); i++) {
         for (int j = 0; j < lenArr; j++)
             if (!(arr[j] % i))
                 count++;
         if (count == lenArr)
-            dividers[k++] = i;
+            max = i;
         count = 0;
     }
-    printf("%d", maxNum(dividers, k));
-    free(dividers);
+    printf("%d", max);
 }
 
 
@@ -49,7 +38,7 @@ int main() {
 
     scanf_s("%d", &lenArr);
 
-    int * arr = (int*)malloc(sizeof(int) * lenArr);
+    int* arr = (int*)malloc(sizeof(int) * lenArr);
 
     scanArr(arr, lenArr);
     printGCD(arr, lenArr);
