a4df8d5391883bea0c1922591e9abf33f12d4cf5
Kirill <148055984+podumai@users.noreply.github.com>
2024-02-16 20:01:10 +0700

    Add files via upload

---
+++ FIRST_COURSE/Skillspace/Module2/1.1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <math.h>
+
+
+void scanArr(int* arr, int lenArr) {
+    for (int i = 0; i < lenArr; i++)
+        scanf_s("%d", &arr[i]);
+}
+
+
+void squareRoot(int* arr, int lenArr) {
+    for (int i = 0; i < lenArr; i++)
+        if (arr[i] == pow(sqrt(arr[i]), 2))
+            printf("%d ", arr[i]);
+}
+
+
+int main() {
+    int lenArr;
+
+    scanf_s("%d", &lenArr);
+
+    int* arr = (int*)malloc(sizeof(int) * lenArr);
+
+    scanArr(arr, lenArr);
+    squareRoot(arr, lenArr);
+    free(arr);
+
+    return 0;
+}
