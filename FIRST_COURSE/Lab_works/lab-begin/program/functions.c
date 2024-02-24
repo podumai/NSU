124bca180f6cbf44ede8f2c6e2ceae5173fb04ac
Kirill <148055984+podumai@users.noreply.github.com>
2024-02-24 20:19:07 +0700

    Update functions.c

--- FIRST_COURSE/Lab_works/lab-begin/program/functions.c
+++ FIRST_COURSE/Lab_works/lab-begin/program/functions.c
@@ -1,14 +1,18 @@
 #include "lab_begin.h"
 
 // Check size of array in range of 1 -- 10
-void check_size(int16_t size) {
+int8_t check_size(int16_t size) {
     if (size < 1 || size > 10) {
         printf("bad input\n");
+
+        return 0;
     }
+
+    return 1;
 }
 
 // Check array in range of INT_MIN -- INT_MAX
-int8_t check_nums(int64_t *in, int16_t size) {
+int8_t check_nums(int64_t* in, int16_t size) {
     for (int16_t i = 0 ; i < size ; i++) {
         if (in[i] > INT32_MAX || in[i] < INT32_MIN) {
             return 0;
@@ -19,7 +23,7 @@ int8_t check_nums(int64_t *in, int16_t size) {
 }
 
 //Check sum of array elements in range INT_MIN -- INT_MAX
-int8_t check_sum(int64_t *in, int16_t size) {
+int8_t check_sum(int64_t* in, int16_t size) {
     int64_t sum;
 
     sum = 0;
@@ -39,7 +43,7 @@ int8_t check_sum(int64_t *in, int16_t size) {
 }
 
 // Sum function for array elements
-int64_t sum(int64_t *in, int16_t size) {
+int64_t sum(int64_t* in, int16_t size) {
     int64_t sum;
 
     sum = 0;
@@ -62,34 +66,39 @@ int8_t check_curr_size(int16_t size, int16_t curr_size) {
     return 1;
 }
 
-// Input function with init array
-int64_t *input(int16_t size) {
-    int64_t *in;
-    int16_t i;
+// Function to initialize an array
+int64_t* init_arr(int16_t size) {
+    int64_t* in;
 
-    in = (int64_t*)calloc(size, sizeof(int64_t));
-    i = 0;
+    in = NULL;
 
-    check_size(size);
-
-    if (!in) {
-        return in;
-    }
-
-    for (i = 0 ; scanf_s("%lld", &in[i]) == 1 && i < size ; i++);
-
-    if (!check_curr_size(size, i)) {
-        free(in);
+    if (size > 0) {
+        in = (int64_t*)calloc(size, sizeof(int64_t));
     }
 
     return in;
 }
 
+// Function for input
+void input(int64_t* in, int16_t size) {
+    int16_t i;
+
+    i = 0;
+
+    if (check_size(size)) {
+        for (i = 0 ; scanf_s("%lld", &in[i]) == 1 && i < size ; i++);
+
+        if (!check_curr_size(size, i)) {
+            free(in);
+        }
+    }
+}
+
 // Output function with several checks
-void output(int64_t *in, int16_t size) {
-    if (in) {
+void output(int64_t* in, int16_t size) {
+    if (check_size(size)) {
         if (check_nums(in, size) && check_sum(in, size)) {
-            printf("%lld\n", sum(in, size));
+        printf("%lld\n", sum(in, size));
         }
         else {
             printf("bad input\n");
