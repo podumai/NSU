b226f308e0994ea94ab9416808b8be0ad1764e1c
Kirill <148055984+podumai@users.noreply.github.com>
2024-02-24 22:01:57 +0700

    Update lab2.h

--- FIRST_COURSE/Lab_works/lab2/program/lab2.h
+++ FIRST_COURSE/Lab_works/lab2/program/lab2.h
@@ -13,41 +13,41 @@
 
 #ifndef INPUT_H_
 #define INPUT_H_
-char *input(void);
+char* input(void);
 #endif
 
 
 #ifndef CHECK_IN_H_
 #define CHECK_IN_H_
-int8_t check_in(char *in);
+int8_t check_in(char* in);
 #endif
 
 
 #ifndef CHECK_NUM_H_
 #define CHECK_NUM_H_
-int8_t check_num(char *in);
+int8_t check_num(char* in);
 #endif
 
 
 #ifndef CHECK_MAX_SWAP_H_
 #define CHECK_MAX_SWAP_H_
-int8_t check_max_swap(char *in);
+int8_t check_max_swap(char* in);
 #endif
 
 
 #ifndef SWAP_H_
 #define SWAP_H_
-void swap(char *in, int32_t index1, int32_t index2);
+void swap(char* in, int32_t index1, int32_t index2);
 #endif
 
 
 #ifndef PERMUTATION_H_
 #define PERMUTATION_H_
-int8_t permutation(char *in);
+int8_t permutation(char* in);
 #endif
 
 
 #ifndef OUTPUT_H_
 #define OUTPUT_H_
-void output(char *in, int32_t N);
+void output(char* in, int32_t N);
 #endif
